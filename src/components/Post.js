6db07a026f88761a2c1845591455a7bd3df9791c
adityavadalkar <adityavadalkar@gmail.com>
2023-01-16 16:41:43 -0800

    Added react files

---
+++ src/components/Post.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+  }}>
+        <div class="card w-75 mb-3">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Post
